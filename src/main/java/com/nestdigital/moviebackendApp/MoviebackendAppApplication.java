9f9dab7bc4ad768942840980c2194a127d8e2074
Elda-Dani <eldadani2001@gmail.com>
2022-10-19 12:29:51 +0530

    git

---
+++ src/main/java/com/nestdigital/moviebackendApp/MoviebackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.moviebackendApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviebackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/moviebackendApp/MoviebackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.moviebackendApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
